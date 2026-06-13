d9ab19e779bbfa24853e4d4eb396bb1f5d60773f
Bohdan Romanov <bytovskyden@gmail.com>
2026-06-13 21:19:56 +0300

    initialize fastapi

--- src/core/config.py
+++ src/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Online Cinema API"
+    PROJECT_VERSION: str = "0.1.0"
     API_V1_STR: str = "/api/v1"
 
     ENVIRONMENT: str = "development"
